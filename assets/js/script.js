7eec1a65f22685d05c4056904bc93e6536dd530a
GusttavoBR <gustavovieira25@live.com>
2025-08-21 20:14:39 -0300

    Projeto Finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,12 @@
 
 let btn = document.querySelector('button')
 let cityCoordinates = []
+let titulo = document.querySelector('.titulo')
+let resultado = document.querySelector('.resultado')
+let tempInfo = document.querySelector('.tempInfo')
+let ventoInfo = document.querySelector('.ventoInfo')
+let ventoPonto = document.querySelector('.ventoPonto')
+let imgElement = document.querySelector('.temp img')
 
 
 
@@ -23,22 +29,40 @@ btn.addEventListener('click', async (e) => {
     let searchInput = document.querySelector('#searchInput')
     cityName = searchInput.value
     console.log(`Cidade dentro do botao: ${cityName}`)
+    let cityData = await getLat_lon(cityName)
+    let lat = cityData.lat
+    let lon = cityData.lon
+    let name = cityData.name
 
-    await getLat_lon()
-    await getWeather()
+    let weatherConditions = await getWeather(lat, lon)
+    let temp = weatherConditions.temp
+    let wind = weatherConditions.wind
+    let windDeg = weatherConditions.windDeg
+    let icon = weatherConditions.icon
     
+
+    resultado.style.display = 'block'
+    titulo.textContent = name
+    tempInfo.textContent = temp
+    ventoInfo.textContent = wind
+    ventoPonto.style.transform = `rotate(${windDeg}deg)`
+    imgElement.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
 })
 
-const getLat_lon = async (lat, lon) => {
+const getLat_lon = async (cityName) => {
     try {
         let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=da254ee7611d9a8c1d9bdaaa8510ee02`)
         let cityResponse = await response.json()
         let lat = cityResponse[0].lat
         let lon = cityResponse[0].lon
-        console.log(`Lat e Lon: ${lat}, ${lon}`)
-        cityCoordinates = [lat, lon]
-        console.log(cityCoordinates)
-        return cityCoordinates
+        let name = cityResponse[0].name
+
+        return {
+            lat: lat,
+            lon: lon,
+            name: name
+        }
     }
     catch (error) {
         console.log(error)
@@ -46,13 +70,25 @@ const getLat_lon = async (lat, lon) => {
 }
 
 
-const getWeather = async () => {
+const getWeather = async (lat, lon) => {
     try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityCoordinates[0]}&lon=${cityCoordinates[1]}&units=metric&lang=pt&appid=da254ee7611d9a8c1d9bdaaa8510ee02
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt&appid=da254ee7611d9a8c1d9bdaaa8510ee02
 `)
         let weatherNow = await response.json()
         console.log(weatherNow)
-        return weatherNow
+        let temp = weatherNow.main.temp
+        let wind = weatherNow.wind.speed
+        let windDeg = weatherNow.wind.deg
+        let icon = weatherNow.weather[0].icon
+
+
+        
+        return {
+            temp: temp,
+            wind: wind,
+            windDeg: windDeg,
+            icon: icon
+        }
 
     }
     catch (error) {
